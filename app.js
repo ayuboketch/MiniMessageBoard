aa6cabe6a13032ba3cd1d269018d97c45c56d0de
Ayub <ayuboketch84@gmail.com>
2025-01-02 11:13:26 +0300

    Necessarity dependencies added

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const morgan = require('morgan');
 const path = require('path');
+const { body, validationResult } = require("express-validator");
 const app = express();
 const PORT = process.env.PORT || 3000;
 
